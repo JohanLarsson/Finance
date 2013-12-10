f4a4e541cdb975fc2add20497684a4a2da79bbfb
Johan Larsson <johan_gavle1@hotmail.com>
2013-12-10 20:43:21 +0100

    Added reference to tests, dunno if it is a good way to test stuff

---
+++ Finance/Finance.Tests/Reference.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Tests
+{
+    /// <summary>
+    /// Implementations with no optimizations to use as references in tests
+    /// </summary>
+    public static class Reference
+    {
+        public static IEnumerable<double> MovingAverage(IList<double> list, int period)
+        {
+            int end = list.Count - period + 1;
+            for (int i = 0; i < end; i++)
+            {
+                yield return list.Skip(i).Take(period).Average();
+            }
+        
+    }
+}
